fe7095a2a64a5a08d05a80664c8a6fbcc9bb0948
Your Name <tonfroy90@gmail.com>
2023-03-30 17:55:41 -0700

    First commit

---
+++ api/routes/products.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /products",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "Handling POST requests to /products",
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "You discovered the special ID",
+    });
+  } else {
+    res.status(200).json({
+      message: "You passed an ID",
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Product'
+    })
+  });
+
+  router.delete("/:productId", (req, res, next) => {
+    res.status(200).json({
+        message: 'Delete Product'
+    })
+  });
+
+module.exports = router;
